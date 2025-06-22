1a1ca22e51fe4f63160448b8fac17b0b27c66c8d
JosefHarvey <josefharveymangaratua@gmail.com>
2025-06-23 00:58:08 +0700

    base design

--- Portofolio-Web/src/assets/component/Main.jsx
+++ Portofolio-Web/src/assets/component/Main.jsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import Experience from "./Experience";
 import Credential from "./Credential";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInbox, faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faInbox, faLocationDot, faPhone, faCode, faWindowMaximize  } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn, faInstagram, faReact, faFigma, faCss, faJs } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Main (){
@@ -39,30 +40,46 @@ export default function Main (){
                                     a Frontend Engineer
                             </div>
                             <div className="leading-[30px] w-[600px] text-lg ">
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                                    <br/> tempor incididunt ut labore et dolore magna aliqua. 
-                                    <br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi       
+                                    I've always been drawn to things I could personalize, from toy cars and games to the digital experiences I build today.  
+                                    <br/> That early curiosity naturally led me to study Computer Science, where I learned to turn ideas into real, interactive products. 
+                                    <br/>Now, as a Front-End Web Developer, I focus on creating intuitive user interfaces with React, while continuously deepening my understanding of full-stack systems.    
                             </div>  
                          </div>
                 
-                        <a href="" className=" text-[#D71515] w-34 rounded-sm pt-[15px] pb-[15px] pr-[35px] pl-[35px] mb-[50px] shadow-lg text-sm font-[700]">View CV </a>
+                        <a href="" className=" text-[#D71515] w-34 rounded-md pt-[15px] pb-[15px] pr-[35px] pl-[35px] mb-[50px] shadow-lg text-sm font-[700] hover:bg-[#D71515] hover:text-white hover:-translate-y-2  transition duration-300">View CV </a>
 
                         <div className="flex ">
                             <div className="flex-1">
                                 <p className="uppercase tracking-widest font-[500] mb-5">Find me on</p>            
                                 <div className="flex gap-10">
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl "></div>
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl"></div>
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl"></div>
+                                    <a href="https://www.linkedin.com/in/josef-harvey/" className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl hover:bg-[#D71515] hover:text-white hover:-translate-y-2  transition duration-300">
+                                        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+                                    </a>
+                                    <a href="https://github.com/JosefHarvey" className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl hover:bg-[#D71515] hover:text-white hover:-translate-y-2  transition duration-300">
+                                        <FontAwesomeIcon icon={faGithub} size="2x" />
+                                    </a>
+                                    <a href="https://www.instagram.com/harveyy_0/" className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl hover:bg-[#D71515] hover:text-white hover:-translate-y-2  transition duration-300">
+                                        <FontAwesomeIcon icon={faInstagram} size="2x" />
+                                    </a>
+                                    
                                 </div>
                             </div>
                             <div className="flex-1">
                                 <p className="uppercase tracking-widest font-[500] mb-5">Best skill on</p>
                                  <div className="flex gap-10">
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl"></div>
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl"></div>
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl"></div>
-                                    <div className="bg-zinc-600 object-cover rounded  w-15 h-15 p-6 shadow-2xl"></div>
+                                    <div className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl text-blue-300 ">
+                                        <FontAwesomeIcon icon={faReact} size="2x" />
+                                    </div>
+                                    <div className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl text-[#D71515]">
+                                        <FontAwesomeIcon icon={faFigma} size="2x" />
+                                    </div>
+                                    <div className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl text-purple-500">
+                                        <FontAwesomeIcon icon={faCss} size="2x" />
+                                    </div>
+                                    <div className="bg-white rounded  w-16 h-16 flex items-center justify-center shadow-2xl text-yellow-300">
+                                        <FontAwesomeIcon icon={faJs} size="2x" />
+                                    </div>
+                                   
                                 </div>
                             </div>
                             
@@ -80,21 +97,41 @@ export default function Main (){
                 <div className=" pb-[100px] pt-[100px]">
                      <div>
                         <p className="text-[#D71515] uppercase tracking-widest text-sm font-[600]">features</p>
-                        <p className="text-6xl font-[700] mb-6">What i <span className="text-8xl font-[500]">♡</span> to Do</p>
+                        <p className="text-6xl font-[700] mb-6">What i <span className="text-8xl font-[500] text-[#D71515]">♡</span> to Do</p>
                     </div>
                     <div className="flex ">
-                        <div className="mr-10  p-5 flex-1.5 shadow-lg rounded-lg hover:shadow-xl">
-                            <p className="text-2xl font-[700] mb-3">Web Development</p>
-                            <p className="leading-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                        <div className="group mr-10  p-10 flex-1.5 shadow-lg rounded-lg hover:shadow-xl hover:bg-[#D71515] bg-white ">
+                           <div className="group-hover:text-white group-hover:-translate-y-5  transition duration-300" >
+                                <div className="pt-5 pb-5">
+                                     <FontAwesomeIcon icon={faCode} size="2x" className="group-hover:text-white text-[#D71515]" />
+                                </div>
+                                <p className="text-2xl font-[700] mb-3">Coding</p>
+                                <p className="leading-[30px] font-[500] ">I enjoy programming because it allows me to turn ideas into functional digital solutions that reflect my personal vision. It’s a creative outlet as well as an intellectual challenge that drives me to keep learning and growing.</p>
+                                <div class="h-1 w-10 rounded-2xl bg-gray-300 my-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                           </div>
                         </div>
-                        <div className="mr-10  p-5 flex-1.5 shadow-lg rounded-lg hover:shadow-xl">
-                            <p className="text-2xl font-[700] mb-3">Web Development</p>
-                            <p className="leading-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                        <div className="group mr-10  p-10 flex-1.5 shadow-lg rounded-lg hover:shadow-xl hover:bg-[#D71515] bg-white ">
+                           <div className="group-hover:text-white group-hover:-translate-y-5  transition duration-300" >
+                                <div className="pt-5 pb-5 ">
+                                     <FontAwesomeIcon icon={faWindowMaximize} size="2x" className="group-hover:text-white text-[#D71515]" />
+                                </div>
+                                <p className="text-2xl font-[700] mb-3">Web Development</p>
+                                <p className="leading-[30px] font-[500] ">II build responsive websites that work well on both mobile and desktop. I enjoy turning ideas into interactive, reliable, and engaging experiences. To me, good design balances usability, performance, and clean visuals.</p>
+                                <div class="h-1 w-10 rounded-2xl bg-gray-300 my-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                           </div>
                         </div>
-                        <div className="mr-10  p-5 flex-1.5 shadow-lg rounded-lg hover:shadow-xl">
-                            <p className="text-2xl font-[700] mb-3">Web Development</p>
-                            <p className="leading-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                        <div className="group mr-10  p-10 flex-1.5 shadow-lg rounded-lg hover:shadow-xl hover:bg-[#D71515] bg-white ">
+                           <div className="group-hover:text-white group-hover:-translate-y-5  transition duration-300" >
+                                <div className="pt-5 pb-5">
+                                     <FontAwesomeIcon icon={faFigma} size="2x" className="text-[#D71515] group-hover:text-white" />
+                                </div>
+                                <p className="text-2xl font-[700] mb-3">UI Design</p>
+                                <p className="leading-[30px] font-[500] ">I enjoy designing interfaces that are both visually appealing and improve the user experience. For me, a good UI is the face of an application it builds trust and guides users naturally.</p>
+                                <div class="h-1 w-10 rounded-2xl bg-gray-300 my-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                           </div>
                         </div>
+                       
+                        
                     </div>
                 </div>
               <hr />
@@ -110,12 +147,14 @@ export default function Main (){
                             <div className="w-auto h-auto mb-3">
                                 <img src={enviro} alt="Photo Enviro" className="w-full h-auto object-cover rounded-md"/>
                             </div>
-                            <div className="flex justify-between mb-6">
-                                <a href="https://enviro-backend.onrender.com/" className="text-[#D71515] text-sm font-[500]">Explore Enviro!</a>
+                            <div className="flex justify-between mb-3 mt-4 items-center">
+                                <a href="https://enviro-backend.onrender.com/" className="text-[#D71515] text-sm font-[500] hover:underline  inline-block group">
+                                Explore Enviro 
+                                <span className="inline-block transition-transform group-hover:translate-x-1"> →</span></a>
                                 <p className="text-sm font-[500]">Web</p>
                             </div>
                             <p className="uppercase text-2xl font-[900] mb-3">Enviro</p>
-                            <p className="leading-[30px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                            <p className="leading-[30px]">Enviro is a web-based learning application focused on educating users about environmental pollution covering its types, causes, and prevention methods.The goal of Enviro is to raise public awareness and make learning about pollution more accessible and engaging.</p>
                         </div>
                     </div>
                 </div>
